d96b3038498a3af7bfbc1949050139521c77e383
gitarojr <catchmegitaro@gmail.com>
2026-05-09 15:19:52 +0700

    gitarojr

---
+++ Controllers/GameController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modul10_103022400142.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> games = new List<Game>
+            {
+            new Game {Id = 1,Name =  "Valorant", Developer = "Riot Games", ReleaseYear = 2020, Genre = "FPS", Rating = 8.5, Platforms = ["PC"], Modes = ["Multiplayer"],isOnline = true, Harga = 0 },
+            //new Game { 1,  "GTA V", "Rockstar Games",2013, "Open World", 9.5, ["PC"], ["Multiplayer"], true, 300000 },
+            //new Game { 1,  "The Witcher 3", "CD Project Red",2015, "RPG", 9.7, ["PC"], ["Single Player"], false, 250000 },
+        };
+
+        [HttpGet]
+        public IEnumerable<List<Game>> Get()
+        {
+            return games;
+        }
+        [HttpGet("{id}")]
+        public Game GetById(int id)
+        {
+            return Games[id];
+        }
+        [HttpPost]
+        public void Post(Game newGame)
+        {
+            Games.Add(newGame);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Games.RemoveAt(id);
+        }
+
+        [HttpPut("{id}")]
+        
+        public void put(int id, Game updatedGame)
+        {
+            var index = Game.FindIndex(g => g.Id == id);
+            if (index != -1)
+                return NotFound();
+                Games[index] = updatedGame;
+            return Ok("update Game");
+
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿namespace Modul10_103022400142
+{
+    public class Game
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+        public string Developer { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public List<string> Platforms { get; set; }
+        public List<string> Modes { get; set; }
+        public bool isOnline { get; set; }
+        public int Harga { get; set; }
+
+        public Game(int id, string name, string developer, int releaseYear, string genre, double rating, List<string> platforms, List<string> modes, bool isOnline, int harga)
+        {
+            Id = id;
+            Name = name;
+            Developer = developer;
+            ReleaseYear = releaseYear;
+            Genre = genre;
+            Rating = rating;
+            Platforms = platforms;
+            Modes = modes;
+            this.isOnline = isOnline;
+            Harga = harga;
+        }
+        public Game()
+        {
+        }
+    }
+}
